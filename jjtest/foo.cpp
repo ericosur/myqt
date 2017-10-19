c95fdf40479b0cbc60dad0853a4f99bd7a73c45d
ericosur <ericosur@gmail.com>
2017-10-19 19:31:51 +0800

    update jjtest, change to_json/from_json by using class with private data member

--- jjtest/foo.cpp
+++ jjtest/foo.cpp
@@ -1,39 +1,38 @@
-#include <iostream>
+#include "foo.h"
 #include "json.hpp"
 
 using namespace nlohmann;
-using namespace std;
-
-struct Person
-{
-    string name;
-    string address;
-    int age;
-};
 
 void to_json(json& j, const Person& p)
 {
-    j = json{{"name", p.name}, {"address", p.address}, {"age", p.age}};
+    j = json{
+        {"name", p.getName()},
+        {"address", p.getAddress()},
+        {"age", p.getAge()}
+    };
 }
 
 void from_json(const json& j, Person& p)
 {
-    p.name = j.at("name").get<std::string>();
-    p.address = j.at("address").get<std::string>();
-    p.age = j.at("age").get<int>();
+    p.setName( j.at("name").get<std::string>() );
+    p.setAddress( j.at("address").get<std::string>() );
+    p.setAge( j.at("age").get<int>() );
 }
 
 void test_foo()
 {
-    Person p = {"Alice Smith", "Minli St., Taipei City", 30};
+    //Person p = {"Alice Smith", "Taipei City", 30};
+    Person p;
+    p.setName("Alice Smith");
+    p.setAddress("Taipei City");
+    p.setAge(30);
+
+    // convert **Person** to **json**
     json j = p;
     cout << j << endl;
 
-    json k = {
-        {"name", "Bob John"},
-        {"address", "New York City"},
-        {"age", 33}
-    };
-    Person p2 = k;
-    cout << p2.name << endl;
+    // convert **json** to **p2**
+    Person p2 = j;
+
+    assert(p == p2);
 }
--- jjtest/foo.h
+++ jjtest/foo.h
@@ -1,6 +1,41 @@
 #ifndef __FOO_H__
 #define __FOO_H__
 
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Person
+{
+public:
+    void setName(const string& s) {
+        name = s;
+    }
+    string getName() const {
+        return name;
+    }
+    void setAddress(const string& s) {
+        address = s;
+    }
+    string getAddress() const {
+        return address;
+    }
+    void setAge(int i) {
+        age = i;
+    }
+    int getAge() const {
+        return age;
+    }
+    bool operator==(const Person& rhs) {
+        return (name == rhs.name) && (address == rhs.address) && (age == rhs.age);
+    }
+private:
+    string name;
+    string address;
+    int age;
+};
+
 void test_foo();
 
 #endif  // __FOO_H__
